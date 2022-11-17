7d1e5f648675b4d6b385ca2caa37149f310adf54
Soichi Ikebe <soichi.ikebe@access-company.com>
2022-11-17 14:40:54 +0900

    Initial Commit

---
+++ ManifestMergerSample/SendActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+
+namespace ManifestMergerSample
+{
+    [Activity(Name = "com.sample.manifestmerger.SendActivity")]
+    public class SendActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_send);  }
+    }
+}
+
